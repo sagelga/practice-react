5abc2bd168c988dd126ec4aae1047ad39dc6ecca
Kunanon Srisuntiroj <son9912@gmail.com>
2023-07-20 12:59:58 +0700

    Add Button and Time components
    Created two new components: Button and Time for the 02-statemap-todolist application. These additions are fundamental blocks to expand the functionality and user interface of the application.

---
+++ 02-statemap-todolist/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>timer</h1>
+        <Time></Time>
+        <Button>Start</Button>
+    </div>
+  );
+}
+
+export default App;
