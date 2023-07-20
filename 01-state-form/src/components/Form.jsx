cd373d1c5e7482038fee550de639712f998cab09
Kunanon Srisuntiroj <son9912@gmail.com>
2023-07-20 13:11:27 +0700

    "Improve form UX by resetting fields after submission"
    The changes made in this commit clear the form fields after it has been submitted. This is accomplished by resetting the state of the 'name', 'age' and 'time' fields after the form's submission function is called. Additionally, unnecessary console.log statements have been removed and the input field values are now controlled by their respective state values ensuring consistency in the application's state and what is displayed to the user.

--- 01-state-form/src/components/Form.jsx
+++ 01-state-form/src/components/Form.jsx
@@ -1,45 +1,50 @@
 import React, { useState } from 'react';
 
 const Form = (props) => {
-    const [name,setName] = useState(' ')
-    const [age, setAge] = useState(0)
-    const [time, setTime] = useState( Date.now())
+    const [name, setName] = useState(' ');
+    const [age, setAge] = useState(0);
+    const [time, setTime] = useState(Date.now());
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        console.log('submit')
-        props.onFormSubmit({name: name, age: age, time: time})
+        console.log('submit');
+        props.onFormSubmit({ name: name, age: age, time: time });
+
+        //     Clear old form
+        setName('');
+        setAge(0);
+        setTime(Date.now());
     };
 
     const nameChange = (event) => {
-        console.log(event.target.value)
-        setName(event.target.value)
-    }
+        // console.log(event.target.value);
+        setName(event.target.value);
+    };
 
     const ageChange = (event) => {
-        console.log(event.target.value)
-        setAge(event.target.value)
-    }
+        // console.log(event.target.value);
+        setAge(event.target.value);
+    };
 
     const timeChange = (event) => {
-        console.log(event.target.value)
-        setTime(event.target.value)
-    }
+        // console.log(event.target.value);
+        setTime(event.target.value);
+    };
 
     return (
         <div>
             <form onSubmit={onFormSubmit}>
                 <div className='name'>
                     <label>Name</label>
-                    <input type='text' onChange={nameChange}></input>
+                    <input type='text' onChange={nameChange} value={name}></input>
                 </div>
                 <div className='age'>
                     <label>Age</label>
-                    <input type='number' onChange={ageChange}></input>
+                    <input type='number' onChange={ageChange} value={age}></input>
                 </div>
                 <div className='time'>
                     <label>Time</label>
-                    <input type='time' onChange={timeChange}></input>
+                    <input type='time' onChange={timeChange} value={time}></input>
                 </div>
                 <input type='submit' value='Submit' />
             </form>
